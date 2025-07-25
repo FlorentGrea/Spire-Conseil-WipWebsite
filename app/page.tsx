c6751208ee1962355d4ef11caf52b03014824a23
FlorentGrea <greaflorent@gmail.com>
2025-07-25 19:22:47 +0200

    too much to see for now

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
       <HomeScreen />
       <ProductsScreen />
       <ReviewsScreen />
-      <TeamScreen />
+      {/*<TeamScreen />*/}
       {/*
         <SubsidiariteScreen />
         <NotreEquipeScreen />
